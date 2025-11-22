83acd9f19902f6fa361686740ff32b9dec71451d
SomeSuperCoder <ithelper9@gmail.com>
2025-11-22 09:18:27 +0300

    Tweaked the formula

--- internal/recommendations/best_of.go
+++ internal/recommendations/best_of.go
@@ -1,6 +1,7 @@
 package recommendations
 
 import (
+	"log"
 	"sort"
 
 	"github.com/SomeSuperCoder/global-chat/models"
@@ -14,9 +15,13 @@ func BestOf(desired *models.UniversalProps, realValues []models.UniversalProps) 
 		diffed := &models.DiffedUniversalProps{}
 
 		diffed.Frequency = +DiffPercentage(desired.Frequency, real.Frequency)
+		log.Println("Freq", diffed.Frequency)
 		diffed.TicketCost = -DiffPercentage(desired.TicketCost, real.TicketCost)
+		log.Println("TicketCost", diffed.TicketCost)
 		diffed.WinRate = +DiffPercentage(desired.WinRate, real.WinRate)
+		log.Println("WinRate", diffed.WinRate)
 		diffed.WinSize = +DiffPercentage(desired.WinSize, real.WinSize)
+		log.Println("WinSize", diffed.WinSize)
 
 		// Diff sum
 		diffSum := diffed.Frequency + diffed.TicketCost + diffed.WinRate + diffed.WinSize
@@ -32,9 +37,9 @@ func BestOf(desired *models.UniversalProps, realValues []models.UniversalProps) 
 	// TODO: ask if there will be a problem
 	sort.Slice(processed, func(i, j int) bool {
 		if processed[i].Diff != processed[j].Diff {
-			return processed[i].Diff > processed[j].Diff
+			return processed[i].Diff < processed[j].Diff
 		}
-		return processed[i].Diff < processed[j].Diff
+		return processed[i].Diff > processed[j].Diff
 	})
 
 	return processed
@@ -58,5 +63,5 @@ func DefaultKs(desired *models.UniversalPropsWithK) {
 // d - desired
 // r - real
 func DiffPercentage(d, r float64) float64 {
-	return (r - d) / d
+	return (d - r) / d
 }
