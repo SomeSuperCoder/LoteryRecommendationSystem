c91287d8f38f4361c11c9d7ba425bc285c19c57e
D0UP1G <doupig1@gmail.com>
2025-11-23 07:54:08 +0300

    added StolotoAPI

--- main.go
+++ main.go
@@ -12,6 +12,11 @@ func main() {
 
 	mux.HandleFunc("POST /suggester", handlers.SuggesterHandler)
 	mux.HandleFunc("POST /best_of", handlers.BestOfHandler)
+	mux.HandleFunc("GET  /draws", handlers.HandleDraws)
+	mux.HandleFunc("GET  /draw", handlers.HandleDraw)
+	mux.HandleFunc("GET  /draw/latest", handlers.HandleDrawLatest)
+	mux.HandleFunc("GET  /draw/prelatest", handlers.HandleDrawPreLatest)
+	mux.HandleFunc("GET  /draw/momental", handlers.HandleMomentalCards)
 
 	log.Println("Server listening on :8090")
 	err := http.ListenAndServe(":8090", mux)
